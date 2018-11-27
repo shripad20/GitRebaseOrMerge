6e793a8bc5016cfce145cc83fe00d84bc01d8e15
Shripad Chidrawar <shripad.cybage@outlook.com>
2018-11-27 16:26:30 +0530

    Initial Commit

---
+++ GitRebaseOrMerge/GitRebaseOrMerge/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  GitRebaseOrMerge
+//
+//  Created by Shripad Chidrawar on 27/11/18.
+//  Copyright © 2018 Cybage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Initial Commit")
+ 
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+
+}
+
